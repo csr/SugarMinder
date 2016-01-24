47aeceeff1b525093941e3077cf9b07ba8272612
Darpan Shah <darpanshah@Darpans-MacBook-Pro.local>
2016-01-24 03:41:21 -0500

    Graph Test

---
+++ PennAppsSpring/PennAppsSpring/BarChartViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BarChartViewController.swift
+//  PennAppsSpring
+//
+//  Created by Darpan Shah on 1/23/16.
+//  Copyright © 2016 Team. All rights reserved.
+//
+
+import Foundation
+import Charts
+import UIKit
+
+class BarChartViewController: UIViewController {
+    
+    @IBOutlet var barChartView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
+
+
